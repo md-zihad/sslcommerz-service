f950be09967b9e888973996e04bc637774cee03e
md-zihad <md.zihad0246@gmail.com>
2024-12-25 19:40:33 +0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const express = require('express')
+const bodyparser = require('body-parser')
+
+const app = express()
+
+require('dotenv').config()
+
+app.use(bodyparser.urlencoded({
+    extended:false
+}))
+app.use
